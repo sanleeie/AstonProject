e643759e70daac6e6a3fa12520fbb51dd3fb75e9
sanleeie <sanleeie@gmail.com>
2024-12-01 00:06:16 +0300

    Задание 1 (добавила с классом Сотрудник)

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -9,6 +9,13 @@ public class Employee {
     int salary;
     int age;
 
-    public Employee(String nameFIO, String position, String email, String phoneNumber, int salary, int age) {
+    public void infoEmployee() {
+        System.out.println("Информация о сотруднике:");
+        System.out.println("ФИО: " + nameFIO);
+        System.out.println("Должность: " + position);
+        System.out.println("Поста: " + email);
+        System.out.println("Телефон: " + phoneNumber);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
     }
 }
