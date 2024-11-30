1d5ba1ce636a7b7e60f2580ec69ad0b1673715b4
sanleeie <sanleeie@gmail.com>
2024-12-01 00:02:57 +0300

    Задание 2 (добавила с классом Сотрудник)

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Employee {
+
+    String nameFIO;
+    String position;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
+
+    public Employee(String nameFIO, String position, String email, String phoneNumber, int salary, int age) {
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Park park = new Park("Парк");
-        Park.Attraction attr1 = park.new Attraction("Колесо", "09:00 - 18:00", 100);
-        Park.Attraction attr2 = park.new Attraction("Горки", "09:00 - 18:00", 300);
+
+        Employee[] empArr = new Employee[5];
+        empArr[0] = new Employee("Вадимов Вадим Вадимович", "Тестировщик", "vadim1@gmail.com", "8998765432", 30000, 30);
+        empArr[1] = new Employee("Сидоров Борис Иванович", "Тестировщик", "boris1@gmail.com", "81928374627", 30000, 27);
+        empArr[2] = new Employee("Глебов Глеб Глебович", "Тестировщик", "gleb2@gmail.com", "86245868344", 25000, 30);
+        empArr[3] = new Employee("Иванов Иван Иванович", "Тестировщик", "ivan53@gmail.com", "6367577222", 40000, 30);
+        empArr[4] = new Employee("Тимуров Тимур Тимурович", "Тестировщик", "timur546@gmail.com", "458957897437", 45000, 30);
+
     }
+
 }
