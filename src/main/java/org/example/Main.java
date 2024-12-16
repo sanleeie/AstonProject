8d38009b1052e6c2f7282d9aa87b54b233bc40f9
sanleeie <sanleeie@gmail.com>
2024-12-16 20:21:17 +0300

    ПЗ Л12. Исключения.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,75 @@
 package org.example;
 
 public class Main {
+    public static int sumArrayElements(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
+        int rows = array.length;
+        if (rows != 4) {
+            throw new MyArraySizeException("Массив должен быть 4x4. Строк сейчас: " + rows);
+        }
+
+        for (String[] row : array) {
+            if (row.length != 4) {
+                throw new MyArraySizeException("Массив должен быть 4x4. В строке: " + row + " - столбцов не 4");
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                String element = array[i][j];
+
+                try {
+                    int number = Integer.parseInt(element);
+                    sum += number;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "] (" + element + ") не является числом");
+                }
+            }
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
 
-        Cat felix = new Cat("Felix");
-        Cat jinni = new Cat("Jinni");
-        Cat vasya = new Cat("Vasya");
-        Dog rex = new Dog("Rex");
-        Dog muha = new Dog("Muha");
-
-        felix.run(100);
-        jinni.run(600);
-        vasya.swim(100);
-        rex.run(500);
-        muha.swim(10);
-
-        System.out.println("Количество котиков: " + Cat.getCountCat());
-        System.out.println("Количество пёсиков: " + Dog.getCountDog());
-        System.out.println("Общее количество питомцев: " + Animal.getCountAnimal());
-
-        Cat[] cats = {
-                new Cat("Феликс"),
-                new Cat("Джини"),
-                new Cat("Вася"),
-                new Cat("Маруся")
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
         };
 
-        Bowl bowl = new Bowl(33);
+        String[][] incorrectSizeArray = {
+                {"1", "1"},
+                {"1", "1"},
+                {"1"}
+        };
 
-        for (Cat cat : cats) {
-            cat.eat(bowl);
-        }
+        String[][] incorrectDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "a", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
-        System.out.println("В миске осталось: " + bowl.getFood() + " еды.");
-        System.out.println("Сытость котиков:");
-        for (Cat cat : cats) {
-            System.out.println(cat.name + ": " + (cat.isSatiety() ? "голодный" : "сытый"));
+
+        try {
+            int sum = sumArrayElements(correctArray);
+            System.out.println("Сумма элементов: " + sum);
+
+            sum = sumArrayElements(incorrectSizeArray);
+            System.out.println("Сумма элементов(неправильный размер): " + sum);
+
+            sum = sumArrayElements(incorrectDataArray);
+            System.out.println("Сумма элементов(неправильные данные): " + sum);
+
+
+        } catch (MyArraySizeException e) {
+            System.err.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.err.println("Ошибка в данных: " + e.getMessage());
         }
-        bowl.addFoodInBowl(15);
-        System.out.println("Добавили 15 единиц еды. Теперь в миске: " + bowl.getFood());
     }
 }
